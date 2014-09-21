10274265675711ee936484c30d6fd813b0a3c7c9
suganya1989 <suganya1989@gmail.com>
2014-09-21 19:01:33 +0100

    server js is not loading

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var io= require('socket.io')();
+
 var port = process.env.PORT || 3838;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
+var io= require('socket.io')(port);
 
