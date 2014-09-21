8fc683fb60a8c8f15ea84ccc8f3ce277940ee8dd
suganya1989 <suganya1989@gmail.com>
2014-09-21 18:59:19 +0100

    socket.io

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-
+var io= require('socket.io')();
 var port = process.env.PORT || 3838;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
