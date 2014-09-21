12f04a6d282fd45fe6a92c2dc5b54a2a6e46f80c
suganya1989 <suganya1989@gmail.com>
2014-09-21 18:31:26 +0100

    server js is not loading

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = process.env.PORT || 3838;
+var port = process.env.PORT || 1337;
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
