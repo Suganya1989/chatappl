ce9bae3bfa08fa42ae1fff29e7562a9806d2529b
suganya1989 <suganya1989@gmail.com>
2014-09-22 19:30:47 +0100

    static file server added

--- server.js
+++ server.js
@@ -1,56 +1,30 @@
-
-var io= require("socket.io")();
-var http= require("http");
-var url= require("url");
-var fs=require("fs");
-var path= require("path");
+var io = require("socket.io")();
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
+var send=require('send');
 
 var port = process.env.PORT || 3838;
 
-var server=http.createServer(function(request,response)
-{
-    var pathn= url.parse(request.url).pathname;
-    var combinePath= __dirname+'\\'+path.basename(pathn);
-    console.log(combinePath);
+var server = http.createServer(function(request, response) {
+    function error(err) {
+        response.statusCode = err.status;
+        response.end(http.STATUS_CODES[err.status]);
+    }
 
-    switch(path.basename(pathn)){
-            case '/':
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.write('hello world');
-                break;
-            case 'chat.html':
-                fs.readFile(combinePath, function(error, data){
-
-                    if (error){
-                        response.writeHead(404);
-                        response.write(error);
-                    }
-                    else{
-                        console.log(data);
-                        response.writeHead(200, {"Content-Type": "text/html"});
-                        response.end(data);
-                    }
-                });
-                break;
-            default:
-                response.writeHead(404);
-                response.write("default");
-                break;
-        }
-
-
+    send(request,request.url,{root:__dirname})
+      .on('error', error)
+      .pipe(response);
 });
 
 server.listen(port);
 
 io.listen(server);
 
-io.on('connection', function(socket){
-    socket.on('msg',function(data)
-    {
+io.on('connection', function(socket) {
+    socket.on('msg', function(data) {
         console.log(data);
         socket.broadcast.emit('broadcastMsg', data);
     })
 })
-
-
