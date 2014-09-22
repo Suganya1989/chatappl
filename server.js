4dca0a1b3009e849c082a50d8f08b20c1c3c3cc9
suganya1989 <suganya1989@gmail.com>
2014-09-22 11:04:38 +0100

    check in

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-var http = require('http');
+var io= require("socket.io")();
+var http= require("http");
+var url= require("url");
+var fs=require("fs");
+var path= require("path");
 
-var port = process.env.PORT || 3838;
-var server=http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
 
-var io= require('socket.io')(server);
 
+var server=http.createServer(function(request,response)
+{
+    var pathn= url.parse(request.url).pathname;
+    var combinePath= __dirname+'\\'+path.basename(pathn);
+    console.log(combinePath);
+
+    switch(path.basename(pathn)){
+            case '/':
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write('hello world');
+                break;
+            case 'chat.html':
+                fs.readFile(combinePath, function(error, data){
+
+                    if (error){
+                        response.writeHead(404);
+                        response.write(error);
+                    }
+                    else{
+                        console.log(data);
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                        response.end(data);
+                    }
+                });
+                break;
+            default:
+                response.writeHead(404);
+                response.write("default");
+                break;
+        }
+
+
+});
+
+server.listen(3838);
+
+io.listen(server);
+
+io.on('connection', function(socket){
+    socket.on('msg',function(data)
+    {
+        console.log(data);
+        socket.broadcast.emit('broadcastMsg', data);
+    })
+})
